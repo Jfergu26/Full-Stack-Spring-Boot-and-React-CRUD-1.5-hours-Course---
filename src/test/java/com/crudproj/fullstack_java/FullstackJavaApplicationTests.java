5506e4e5c56fa01d22b4da8a04dafa320e62d90a
Jaryn Ferguson <Jaryntf@gmail.com>
2024-07-15 11:31:26 -0400

    init
    init

---
+++ src/test/java/com/crudproj/fullstack_java/FullstackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudproj.fullstack_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
